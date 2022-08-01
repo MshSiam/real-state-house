21d10d4f76b74986065c4d7d60ed7dbaced2a674
MshSiam <siyamkenway@gmail.com>
2022-08-01 13:12:55 +0600

    done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react"
 
 const Footer = () => {
-  return <div>Footer</div>;
-};
+  return (
+    <footer className="bg-black py-8 text-center text-white ">
+      <div className="container">
+        Copyright &copy; 2022. All Right Reserver by Muhammad Siam
+      </div>
+    </footer>
+  )
+}
 
-export default Footer;
+export default Footer
